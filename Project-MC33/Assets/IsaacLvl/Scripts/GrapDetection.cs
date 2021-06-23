7c13917e597efefcb98c8f3b42d9f1f41fe51ddc
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-06-23 16:28:21 +0200

    Revert "Deleted isaac lvl"
    This reverts commit a3f39ca0491e1b230565359e287581bd53f87674.

---
+++ Project-MC33/Assets/IsaacLvl/Scripts/GrapDetection.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapDetection : MonoBehaviour
+{
+    public bool grapOn = false;
+    public Camera mainCamera;
+    public LineRenderer _lineRenderer;
+    public DistanceJoint2D _distanceJoint;
+    public Vector2 grapPosO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _distanceJoint.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x") && grapOn == false)
+        {
+            grapOn = false;
+        }
+        if (Input.GetKeyDown("x") && grapOn == true)
+        {
+            Vector2 grapPos = grapPosO;
+            _lineRenderer.SetPosition(0, grapPos);
+            _lineRenderer.SetPosition(1, transform.position);
+            _distanceJoint.connectedAnchor = grapPos;
+            _distanceJoint.enabled = true;
+            _lineRenderer.enabled = true;
+        }
+        else if (Input.GetKeyUp("x"))
+        {
+            _distanceJoint.enabled = false;
+            _lineRenderer.enabled = false;
+            grapOn = false;
+        }
+        if (_distanceJoint.enabled) 
+        {
+            _lineRenderer.SetPosition(1, transform.position);
+        }
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.layer == LayerMask.NameToLayer("Grap"))
+        {
+            //Debug.Log("Grap!");
+            grapPosO = col.transform.position;
+            grapOn = true;
+        }
+        else
+        {
+            grapOn = false;
+        }
+    }
+}
---
+++ Project-MC33/Assets/IsaacLvl/Scripts/MovementFirst.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementFirst : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public bool isGrounded = false;
+    public bool doubleJump;
+    [SerializeField] private LayerMask GroundMask;
+    public Rigidbody2D rb;
+    public float runSpeed = 40f;
+    public float jumpVel = 10f;
+    public CapsuleCollider2D capsCol;
+    float horizontalMove = 0f;
+    public Collider2D attachCol;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(IsGrounded())
+        {
+            doubleJump = true;
+        }
+                
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if(IsGrounded())
+            {
+                rb.velocity = (Vector2.up) * jumpVel;
+            } else
+            {
+                if(doubleJump)
+                {
+                    rb.velocity = (Vector2.up) * (jumpVel);
+                    doubleJump = false;
+                }
+            }
+        }
+             
+    }
+   
+    private bool IsGrounded()
+    {
+       RaycastHit2D rayCasthit2D = Physics2D.BoxCast(capsCol.bounds.center, capsCol.bounds.size, 0f, Vector2.down, 1f, GroundMask);
+        if(rayCasthit2D.collider != null)
+        {
+            isGrounded = true;
+            doubleJump = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+       return rayCasthit2D.collider != null;
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false,false);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.layer == LayerMask.NameToLayer("resetJump"))
+        {
+            doubleJump = true;
+        }
+    }
+}
