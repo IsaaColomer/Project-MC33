20eab3ea9bec242a6cce28b756a017472791ca07
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2021-06-24 19:07:56 +0200

    Revert "a"
    This reverts commit 0fba75913c9736b8bbc639b3322026bec17afc87.

--- Project-MC33/Assets/Bullet.cs
+++ Project-MC33/Assets/Bullet.cs
@@ -6,15 +6,10 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
-    private Rigidbody2D player;
-    public GameObject playerO;
-    public Vector3 savedPosition;
-    public bool boxHitted = false;
 
     Weapon count;
     void Start()
     {
-       player = playerO.GetComponent<Rigidbody2D>();
        rb.velocity = transform.right * speed;
     }
     void OnTriggerEnter2D(Collider2D col)
@@ -36,17 +31,6 @@ public class Bullet : MonoBehaviour
         if(col.gameObject.layer == LayerMask.NameToLayer("Spikes"))
         {
             Debug.Log("Spike Detected!");
-            Destroy(gameObject);
-            count2.bulletCount--;
-        }
-        if(col.gameObject.layer == LayerMask.NameToLayer("Box"))
-        {
-            GameObject playerNowPos = GameObject.Find("PlayerGeneral");
-            FallRestart playerNowPos2 = count.GetComponent<FallRestart>();
-
-            playerNowPos2.startPos = player.transform.position;
-            boxHitted = true;
-            Debug.Log("Box Detected!");
             Destroy(gameObject);
             count2.bulletCount--;
         }
--- Project-MC33/Assets/FirstWorld/Scripts/FallRestart.cs
+++ Project-MC33/Assets/FirstWorld/Scripts/FallRestart.cs
@@ -113,5 +113,7 @@ public class FallRestart : MonoBehaviour
                 mainCamera.transform.position = new Vector3(other.transform.position.x,0f,-9.8f);
                 startPos = rb.transform.position;      
             }
+           
+
     }
 }
