1cb72dcdc07fde2ace20285ce922a82b04a27ff8
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-08-01 12:05:01 +0200

    brand new lvls!

---
+++ Project-MC33/Assets/IsaacLvl/Scripts/Break2.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break2 : MonoBehaviour
+{
+    public float time;
+    public GameObject platform;
+    public SpriteRenderer me;
+    public Collider2D breakCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.02f;
+        breakCol = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject grap = GameObject.Find("PlayerGeneral");
+        FallRestart grap2 = grap.GetComponent<FallRestart>();
+
+        if(grap2.dead == true)
+        {
+            me.color = new Color(1f,1f,1f,1f);
+            breakCol.enabled = true;
+            time = 0.02f;
+        }
+
+    }
+    void OnCollisionStay2D(Collision2D col)
+    {
+        me.color = new Color(1f,1f,1f,time);
+        if(time <= 0)
+        {
+            breakCol.enabled = !breakCol.enabled;
+        }
+
+        if(col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("PlayerOnPlatform");
+            time -= Time.deltaTime;
+        }
+        Debug.LogFormat("Time: {0}", time);
+    }
+}
