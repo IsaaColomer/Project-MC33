14937b362942813c6e90b7c9533c1c4508453fad
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-08-01 14:00:19 +0200

    idk error

--- Project-MC33/Assets/FirstWorld/Scripts/CheckPoints.cs
+++ Project-MC33/Assets/FirstWorld/Scripts/CheckPoints.cs
@@ -7,9 +7,12 @@ public class CheckPoints : MonoBehaviour
     public Vector3 startPos2;
     public Vector3 restartPos2;
     public Rigidbody2D chRb;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         restartPos2 = startPos2;
     }
 
@@ -19,6 +22,12 @@ public class CheckPoints : MonoBehaviour
         
     }
 
+    // void ChangeSprite()
+    // {
+        
+    // }
+    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
@@ -26,6 +35,7 @@ public class CheckPoints : MonoBehaviour
             GameObject pos01 = GameObject.Find("PlayerGeneral");
             FallRestart pos02 = pos01.GetComponent<FallRestart>();
             pos02.startPos = chRb.transform.position;
+            spriteRenderer.sprite = newSprite;
         }
     }
 }
