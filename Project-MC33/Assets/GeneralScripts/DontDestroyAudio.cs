a03e3a128427aa694aa71c32e49f0e1a5b7ea785
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-08-05 18:15:24 +0200

    music problem fixed

--- Project-MC33/Assets/GeneralScripts/DontDestroyAudio.cs
+++ Project-MC33/Assets/GeneralScripts/DontDestroyAudio.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyAudio : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static DontDestroyAudio musicManagerInstance;
     void Start()
     {
-
+        AudioSource audioSource = GetComponent<AudioSource>();
+        Scene level = SceneManager.GetActiveScene();
+        string levelName = level.name;
+        DontDestroyOnLoad(this);
+        if(musicManagerInstance == null)
+        {
+            musicManagerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        audioSource.Play();
     }
 }
