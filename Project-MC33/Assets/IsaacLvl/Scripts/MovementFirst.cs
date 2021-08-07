cd234148f91a1585f80432602cfd2484cf070474
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-08-07 18:03:59 +0200

    aaa

--- Project-MC33/Assets/IsaacLvl/Scripts/MovementFirst.cs
+++ Project-MC33/Assets/IsaacLvl/Scripts/MovementFirst.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MovementFirst : MonoBehaviour
 {
     public CharacterController2D controller;
